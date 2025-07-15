5760c59b61d64eec26086dddc254f7d8635c59d3
mikkosuokas <mikesuokas@gmail.com>
2025-07-15 09:50:42 +0300

    Done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,16 +1,52 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: msuokas <msuokas@student.hive.fi>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/10 16:53:52 by msuokas           #+#    #+#             */
-/*   Updated: 2025/07/10 16:59:28 by msuokas          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "replacer.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-
+	if (argc != 4)
+	{
+		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>\n";
+		return 1;
+	}
+	std::string input_filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1 cannot be an empty string.\n";
+		return 1;
+	}
+	std::ifstream input_file(input_filename);
+	if (!input_file.is_open())
+	{
+		std::cerr << "Error: Could not open input file.\n";
+		return 1;
+	}
+	std::string output_filename = input_filename + ".replace";
+	std::ofstream output_file(output_filename);
+	if (!output_file.is_open())
+	{
+		std::cerr << "Error: Could not create output file.\n";
+		return 1;
+	}
+	std::string line;
+	while (std::getline(input_file, line))
+	{
+		std::string result;
+		std::size_t pos = 0;
+		std::size_t found;
+		while ((found = line.find(s1, pos)) != std::string::npos)
+		{
+			result += line.substr(pos, found - pos);
+			result += s2;
+			pos = found + s1.length();
+		}
+		result += line.substr(pos);
+		output_file << result << '\n';
+	}
+	input_file.close();
+	output_file.close();
+	return 0;
 }
---
+++ ex04/replacer.hpp
@@ -0,0 +1,7 @@
+#ifndef REPLACER_HPP
+#define REPLACER_HPP
+
+#include <iostream>
+#include <fstream>
+
+#endif
